1141ffe29093c5cb00a88ec6b010ab3bd9d48961
Rildo Maciel <77412852+rildodev@users.noreply.github.com>
2021-10-21 22:09:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const LinksSocialMedia = {
   github: 'rildodev',
   youtube: 'RildoMaciel',
   facebook: 'rildomaciel',
-  instagram: 'rildoberto',
+  instagram: 'rildoberto/',
   twitter: 'rildoberto'
 }
 
