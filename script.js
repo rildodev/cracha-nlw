231d13a2d34bed9f0e70907ccb5b6c150e1970f8
Rildo Maciel <77412852+rildodev@users.noreply.github.com>
2021-10-24 11:48:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const LinksSocialMedia = {
   github: 'rildodev',
-  youtube: 'RildoMaciel',
-  facebook: 'rildomaciel',
-  instagram: 'rildoberto/',
-  twitter: 'rildoberto'
+  linkedin: '/in/rildo-maciel-berto-da-silva-4430a9221/',
+  instagram: 'rildoberto',
 }
 
 function changeSocialMediaLinks() {
@@ -13,6 +11,7 @@ function changeSocialMediaLinks() {
     li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
   }
 }
+
 changeSocialMediaLinks()
 
 /* API do GITHUB */
@@ -20,13 +19,15 @@ changeSocialMediaLinks()
 function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
-  fetch(url).then(response => response.json())/* pega info da api na json do git*/
-  .then(data => {
-    userName.textContent = data.name
-    userBio.textContent = data.bio
-    userLink.href = data.html_url
-    userAvatar.src = data.avatar_url
-    userNick.textContent = data.login
-  })
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userAvatar.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
 }
+
 getGitHubProfileInfos()
